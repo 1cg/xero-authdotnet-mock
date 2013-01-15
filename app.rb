794845e3fed187d52c05b2795a0db6c62975d919
Carson Gross <carsongross@gmail.com>
2013-01-15 11:47:05 -0800

    implement redirect logic

--- app.rb
+++ app.rb
@@ -1,13 +1,36 @@
 require 'sinatra'
 
-get '/' do 
+get '/r' do 
+  redirect = params[:url]
+  if redirect
+    redirect = URI.decode(redirect)
 """
 <html>
   <body>
     <script type='text/javascript'>
-      window.location='http://www.google.com';
+      window.location='#{redirect}';
     </script>
   </body>
 </html>
 """
+  else
+"""
+<html>
+  <body>
+    <h1>No Redirect Supplied</h1>
+  </body>
+</html>
+"""
+  end
 end
+
+get '/' do 
+"""
+<html>
+  <body>
+    <h1>Hello Xero</h1>
+  </body>
+</html>
+"""
+end
+
